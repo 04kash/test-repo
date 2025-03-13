fa98b4f72f79d202b140ec1f208464cdf3259a09
Kashish Mittal <113269381+04kash@users.noreply.github.com>
2025-03-13 15:25:30 -0400

    Update list-workspaces.js

--- scripts/list-workspaces.js
+++ scripts/list-workspaces.js
@@ -1,9 +1,25 @@
-const fs = require('fs-extra');
-const path = require('path');
+#!/usr/bin/env node
+/*
+ * Copyright 2024 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-const resolve = path.resolve;
+import fs from 'fs-extra';
+import { resolve } from 'path';
+import * as url from 'url';
 
-// Use the built-in __dirname provided by Node.js
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 const EXCLUDED_WORKSPACES = ['noop', 'repo-tools'];
 
 /**
@@ -13,8 +29,8 @@ const EXCLUDED_WORKSPACES = ['noop', 'repo-tools'];
  *                             excluding directories listed in EXCLUDED_WORKSPACES.
  * @throws {Error} If there are filesystem errors reading the directory
  */
-async function listWorkspaces() {
-  const rootPath = resolve(__dirname, '..');  // Use existing __dirname
+export async function listWorkspaces() {
+  const rootPath = resolve(__dirname, '..');
   const workspacePath = resolve(rootPath, 'workspaces');
 
   return (await fs.readdir(workspacePath, { withFileTypes: true }))
@@ -22,6 +38,4 @@ async function listWorkspaces() {
     .map(w => w.name);
 }
 
-// Export the function in CommonJS format
-module.exports = { listWorkspaces };
 
