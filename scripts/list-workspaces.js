84eabfd8ec86fdc470522501321e9fce5913c640
Kashish Mittal <113269381+04kash@users.noreply.github.com>
2025-03-13 15:22:45 -0400

    Update list-workspaces.js

--- scripts/list-workspaces.js
+++ scripts/list-workspaces.js
@@ -1,27 +1,13 @@
-#!/usr/bin/env node
-/*
- * Copyright 2024 The Backstage Authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 const fs = require('fs-extra');
 const path = require('path');
 const url = require('url');
 
 const resolve = path.resolve;
 
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+// Use __dirname directly in CommonJS
+const __dirname = path.resolve();
+
+// List of excluded workspaces
 const EXCLUDED_WORKSPACES = ['noop', 'repo-tools'];
 
 /**
@@ -31,7 +17,7 @@ const EXCLUDED_WORKSPACES = ['noop', 'repo-tools'];
  *                             excluding directories listed in EXCLUDED_WORKSPACES.
  * @throws {Error} If there are filesystem errors reading the directory
  */
-export async function listWorkspaces() {
+async function listWorkspaces() {
   const rootPath = resolve(__dirname, '..');
   const workspacePath = resolve(rootPath, 'workspaces');
 
@@ -39,3 +25,6 @@ export async function listWorkspaces() {
     .filter(w => w.isDirectory() && !EXCLUDED_WORKSPACES.includes(w.name))
     .map(w => w.name);
 }
+
+// Export the function in CommonJS format
+module.exports = { listWorkspaces };
